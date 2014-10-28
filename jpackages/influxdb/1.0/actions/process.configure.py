f338f19f1f63090b6dc2ee55e1907f67b539779d
despiegk <kristof@incubaid.com>
2014-10-28 15:05:26 +0200

    influxdb

--- jpackages/influxdb/1.0/actions/process.configure.py
+++ jpackages/influxdb/1.0/actions/process.configure.py
@@ -1,10 +1,10 @@
 def main(j,jp):
     name = "influxdb"
     workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'influxdb')
-    cmd = "./influxdb"
+    cmd = "%s/influxdb"%workingdir
     cfgfile = j.system.fs.joinPaths(j.dirs.cfgDir, 'influxdb', 'config.toml')
     args = "-config=%s" % cfgfile
     startstoptimeout = 60
-    processfilterstr = "./influxdb -config"
+    processfilterstr = "influxdb -config"
     j.tools.startupmanager.addProcess(name, cmd, workingdir=workingdir, args=args, domain="serverapps",jpackage=jp,ports=[8086,8090,8083],priority=1,\
         check=True,timeoutcheck=startstoptimeout,isJSapp=0,processfilterstr=processfilterstr,stats=True,upstart=True)
