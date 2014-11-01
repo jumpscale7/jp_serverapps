5dba6aa2458b2dbf63a3127bb381544111c84a2d
despiegk <kristof@incubaid.com>
2014-11-01 16:14:58 +0200

    lots of improvements: logging/events ...

--- jpackages/oss/1.0/actions/install.prepare.py
+++ jpackages/oss/1.0/actions/install.prepare.py
@@ -9,7 +9,7 @@ def main(j,jp):
     jp2=j.packages.findNewest("jumpscale","portal")
     if not jp2.isInstalled(instance=instance):
         j.events.inputerror_critical("Could not find portal instance with name: %s, please install"%instance)
-        # jp2.install(hrddata={"redis.name":"production","redis.port":"7768","redis.disk":"1","redis.mem":400},instance="$(cloudrobot.portal.instance)")
+        # jp2.install(hrddata={"redis.name":"production","redis.port":"9999","redis.disk":"1","redis.mem":400},instance="$(cloudrobot.portal.instance)")
 
     j.system.fs.removeDirTree("$base/apps/portals/%s/base/test__taskmanager/"%instance)
 
